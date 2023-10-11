753f0978973a691d71b9abb20744d5a65fe0c53e
Gonzalo <gonzalosuarez9977@gmail.com>
2023-10-11 13:30:18 -0300

    Cambio Pokeball

--- src/components/PokeTeamEmptyCard.jsx
+++ src/components/PokeTeamEmptyCard.jsx
@@ -3,7 +3,7 @@ import "../css/PokeTeamEmptyCard.css";
 export default function PokeTeamEmptyCard() {
   return (
     <div className="col-11 d-flex justify-content-center align-items-center rounded poke_team_empty_card m-1">
-      <img className="col-6" src="../public/pokeBall.png"/>
+      <img className="col-6" src="../../public/pokeBall.png"/>
     </div>
   );
 }
