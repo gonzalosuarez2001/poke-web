68fd7dbb99b0f68c09e2079d66868f345f4dbfed
Gonzalo <gonzalosuarez9977@gmail.com>
2023-10-17 01:21:31 -0300

    Actualizo

--- src/components/pokeInfo/PokeInfoCard.jsx
+++ src/components/pokeInfo/PokeInfoCard.jsx
@@ -8,12 +8,17 @@ export default function PokeInfoCard(props) {
       <div className="col-9 col-sm-5 col-md-3 col-xl-2 col-xxl-2 text-center rounded m-2 px-2 poke_info_card">
         <h4 className="pt-3 text-secondary fw-bold">{props.name}</h4>
         <div>
-          <img className="pt-3 mx-2 col-5" src={props.front_sprite} />
-          <img className="pt-3 mx-2 col-5" src={props.back_sprite} />
+          <img className="pt-3 mx-2 col-6" src={props.front_sprite} />
+          <img className="pt-3 mx-2 col-6" src={props.back_sprite} />
         </div>
-        <div className="row justify-content-center py-3">
+        <div className="row justify-content-center pt-3">
           {props.types.map((type, index) => {
             return <PokeCardTypes key={index} type={type.type.name} />;
+          })}
+        </div>
+        <div className="row justify-content-center py-3">
+          {props.stats.map((stat, index) => {
+            return <p key={index}>{stat.stat.name.toUpperCase()} {stat.base_stat}</p>;
           })}
         </div>
       </div>
